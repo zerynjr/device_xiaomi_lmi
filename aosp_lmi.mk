4d763a9eb638bc3f09a24203f0532a3455c1844d
zerynjr <hiphopvr@gmail.com>
2020-10-09 15:55:02 -0300

    lmi: PE bringup

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -8,10 +8,12 @@
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := aosp_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
